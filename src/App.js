07a367bf4e32d44b8073dbeafffe8e596a912214
unknown <cse.21bcse06@silicon.ac.in>
2023-07-25 16:55:34 +0530

    add components and pages

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import './App.css';
+import Navbar from './Components/Navbar/Navbar';
+import Homepage from './Pages/Homepage/Hpmepage';
+
+function App() {
+  return (
+    <>
+    <div className='App'>
+      <Navbar/>
+      <Homepage/>
+    </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/Homepage/Hpmepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomeContect from '../../Components/HomeContent/HomeContent'
+
+function Homepage() {
+  return (
+    <div>
+      <HomeContect/>
+    </div>
+  )
+}
+
+export default Homepage
