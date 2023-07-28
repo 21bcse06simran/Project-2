4f95ef83278171bf4e1babd5ec4924de6413f177
unknown <cse.21bcsc01@gmail.com>
2023-07-29 02:44:10 +0530

    My final

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,17 +1,1 @@
-import React from 'react'
-import HomeContect from '../../Components/HomeContent/HomeContect'
-import Signup from '../../Components/Signup/Signup'
-import Connect from '../../Components/Connect/Connect'
-
-function Homepage() {
-  return (
-    <div>
-      <HomeContect/>
-      <Signup/>
-      <Connect/>
-    </div>
-  )
-}
-
-export default Homepage
 
