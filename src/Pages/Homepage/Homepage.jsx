4d2d9b42be77a92e4d2d509e01858dc7df43322f
21bcse06simran <138780970+21bcse06simran@users.noreply.github.com>
2023-07-29 13:12:15 +0530

    Update Homepage.jsx

--- src/Pages/Homepage/Homepage.jsx
+++ src/Pages/Homepage/Homepage.jsx
@@ -1,10 +1,16 @@
 import React from 'react'
-import HomeContect from '../../Components/HomeContent/HomeContent'
+import HomeContect from '../../Components/HomeContent/HomeContect'
+//import Navbar from '../../Components/Navbar/Navbar'
+import Signup from '../../Components/Signup/Signup'
+import Connect from '../../Components/Connect/Connect'
 
 function Homepage() {
   return (
     <div>
+      {/*<Navbar/>*/}
       <HomeContect/>
+      <Signup/>
+      <Connect/>
     </div>
   )
 }
